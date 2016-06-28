3c3c7461e91d6be8a3b9c33b7358be63c01b26e9
Nikos Verschore <nikosverschore@gmail.com>
2016-06-28 13:53:28 +0200

    Sign the newest release (Made conform to PatrolServer 1.1.0)

--- compiled.sh
+++ compiled.sh
@@ -234,14 +234,15 @@ function ApiUserRegister {
 	local EMAIL
 	local PASSWORD
 	local OUTPUT
-	local AUTHED
-	local ERRORS
+	local ERROR
 	local USER
+	local KEY
+	local SECRET
 
 	EMAIL=$(Urlencode "$1")
 	PASSWORD=$(Urlencode "$2")
 
-	OUTPUT=$(wget -t2 -T6 --keep-session-cookies --save-cookies "$COOKIES" -qO- "${MY_HOME}/api/user/register" --post-data "email=$EMAIL&password=$PASSWORD&password_confirmation=$PASSWORD")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/users" --post-data "email=$EMAIL&password=$PASSWORD&password_confirmation=$PASSWORD")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -249,29 +250,30 @@ function ApiUserRegister {
 		exit 77
 	fi
 
-	AUTHED=$(echo "$OUTPUT" | json | grep '^\["authed"\]' | cut -f2-)
-	ERRORS=$(echo "$OUTPUT" | json | grep '^\["errors",[0-9]\]' | cut -f2-)
-	USER=$(echo "$OUTPUT" | json | grep '^\["user"\]' | cut -f2-)
+	ERROR=$(echo "$OUTPUT" | json | grep '^\["error","code"\]' | cut -f2-)
+	USER=$(echo "$OUTPUT" | json | grep '^\["data","user"\]' | cut -f2-)
+	KEY=$(echo "$OUTPUT" | json | grep '^\["data","api_credentials","key"\]' | cut -f2-)
+	SECRET=$(echo "$OUTPUT" | json | grep '^\["data","api_credentials","secret"\]' | cut -f2-)
 
-	echo "${AUTHED:-false}"
-	echo "${ERRORS:-false}"
+	echo "${ERROR:-false}"
 	echo "${USER:-false}"
+	echo "${KEY:-false}"
+	echo "${SECRET:-false}"
 }
 
 function ApiUserLogin {
 	local EMAIL
 	local PASSWORD
 	local OUTPUT
-	local AUTHED
-	local ERRORS
+	local ERROR
 	local USER
-	local CRITICAL
-	local TYPE
+	local KEY
+	local SECRET
 
 	EMAIL=$(Urlencode "$1")
 	PASSWORD=$(Urlencode "$2")
 
-	OUTPUT=$(wget -t2 -T6 --keep-session-cookies --save-cookies "$COOKIES" -qO- "${MY_HOME}/api/user/login" --post-data "email=$EMAIL&password=$PASSWORD")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/api/user/request_api_credentials" --post-data "email=$EMAIL&password=$PASSWORD")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -279,17 +281,15 @@ function ApiUserLogin {
 		exit 77
 	fi
 
-	AUTHED=$(echo "$OUTPUT" | json | grep '^\["authed"\]' | cut -f2-)
-	ERRORS=$(echo "$OUTPUT" | json | grep '^\["errors",[0-9]\]' | cut -f2-)
+	ERROR=$(echo "$OUTPUT" | json | grep '^\["errors","code"\]' | cut -f2-)
 	USER=$(echo "$OUTPUT" | json | grep '^\["user"\]' | cut -f2-)
-	CRITICAL=$(echo "$OUTPUT" | json | grep '^\["critical"\]' | cut -f2-)
-	TYPE=$(echo "$OUTPUT" | json | grep '^\["type"\]' | cut -f2-)
+	KEY=$(echo "$OUTPUT" | json | grep '^\["data","api_credentials","key"\]' | cut -f2-)
+	SECRET=$(echo "$OUTPUT" | json | grep '^\["data","api_credentials","key"\]' | cut -f2-)
 
-	echo "${CRITICAL:-false}"
-	echo "${TYPE:-false}"
-	echo "${AUTHED:-false}"
-	echo "${ERRORS:-false}"
+	echo "${ERROR:-false}"
 	echo "${USER:-false}"
+	echo "${KEY}:-false}"
+	echo "${SECRET}:-false}"
 }
 
 function ApiServerExists {
@@ -301,7 +301,7 @@ function ApiServerExists {
 
 	HOST=$(Urlencode "$1")
 
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/api/server/exists?host=$HOST")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/api/server/exists?host=$HOST")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -330,7 +330,7 @@ function ApiServerCreate {
 	SECRET=$(Urlencode "$2")
 	HOSTNAME=$(Urlencode "$3")
 	
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/servers?key=$KEY&secret=$SECRET" --post-data "domain=$HOSTNAME")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/servers?key=$KEY&secret=$SECRET" --post-data "domain=$HOSTNAME")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -353,7 +353,7 @@ function ApiServerToken {
 	
 	HOSTNAME=$(Urlencode "$1")
 	
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/request_verification_token?domain=$HOSTNAME")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/request_verification_token?domain=$HOSTNAME")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -381,7 +381,7 @@ function ApiVerifyServer {
 	SERVER_ID=$(Urlencode "$3")
 	TOKEN=$(Urlencode "$4")
 	
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/servers/${SERVER_ID}/verify?key=$KEY&secret=$SECRET" --post-data "token=$TOKEN")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/servers/${SERVER_ID}/verify?key=$KEY&secret=$SECRET" --post-data "token=$TOKEN")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -426,7 +426,7 @@ function ApiServerPush {
 	echo "$SOFTWARE" >> "$POSTFILE"
 
 
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/servers/${SERVER_ID}/software_bucket/$BUCKET?key=$KEY&secret=$SECRET&scope=silent" --post-file $POSTFILE)
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/servers/${SERVER_ID}/software_bucket/$BUCKET?key=$KEY&secret=$SECRET&scope=silent" --post-file $POSTFILE)
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -439,38 +439,6 @@ function ApiServerPush {
 	echo "${ERROR:-false}"
 }
 
-function ApiKeySecret {
-	local OUTPUT
-	local KEY
-	local SECRET
-
-	OUTPUT=$(wget -t2 -T6 --load-cookies "$COOKIES" -qO- "${MY_HOME}/api/user/api_credentials")
-
-	if [ "$OUTPUT" == "" ]
-	then
-		echo "> patrolserver.com is not reachable, contact us (8)" >&2
-		exit 77
-	fi
-
-	KEY=$(echo "$OUTPUT" | json | grep '^\[0,"key"\]' | cut -f2- | sed -e 's/^"//'  -e 's/"$//')
-	SECRET=$(echo "$OUTPUT" | json | grep '^\[0,"secret"\]' | cut -f2- | sed -e 's/^"//'  -e 's/"$//')
-
-	echo "${KEY:-false}"
-	echo "${SECRET:-false}"
-}
-
-function ApiCreateKeySecret {
-	local OUTPUT
-
-	OUTPUT=$(wget -t2 -T6 --load-cookies "$COOKIES" -qO- "${MY_HOME}/api/user/api_credentials" --post-data "not=used")
-
-	if [ "$OUTPUT" == "" ]
-	then
-		echo "> patrolserver.com is not reachable, contact us (9)" >&2
-		exit 77
-	fi
-}
-
 function ApiServers {
 	local KEY
 	local SECRET
@@ -481,7 +449,7 @@ function ApiServers {
 	KEY=$(Urlencode "$1")
 	SECRET=$(Urlencode "$2")
 
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/servers?key=$KEY&secret=$SECRET")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/servers?key=$KEY&secret=$SECRET")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -508,7 +476,7 @@ function ApiSoftware {
 	SECRET=$(Urlencode "$2")
 	SERVER_ID=$(Urlencode "$3")
 
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/servers/$SERVER_ID/software?key=$KEY&secret=$SECRET&scope=exploits")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/servers/$SERVER_ID/software?key=$KEY&secret=$SECRET&scope=exploits")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -559,7 +527,7 @@ function ApiServerIsScanning {
 	SECRET=$(Urlencode "$2")
 	SERVER_ID=$(Urlencode "$3")
 
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/servers/$SERVER_ID/isScanning?key=$KEY&secret=$SECRET")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/servers/$SERVER_ID/isScanning?key=$KEY&secret=$SECRET")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -586,7 +554,7 @@ function ApiUserChange {
 	SECRET=$(Urlencode "$2")
 	EMAIL=$(Urlencode "$3")
 
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/user/update?key=$KEY&secret=$SECRET" --post-data "email=$EMAIL")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/user/update?key=$KEY&secret=$SECRET" --post-data "email=$EMAIL")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -609,7 +577,7 @@ function ApiUserRemove {
 	KEY=$(Urlencode "$1")
 	SECRET=$(Urlencode "$2")
 
-	OUTPUT=$(wget -t2 -T6 -qO- "${MY_HOME}/extern/api/user/delete?key=$KEY&secret=$SECRET" --post-data "not=used")
+	OUTPUT=$(wget -t2 -T6 --header="X-PS-Bash: 1" -qO- "${MY_HOME}/extern/api/user/delete?key=$KEY&secret=$SECRET" --post-data "not=used")
 
 	if [ "$OUTPUT" == "" ]
 	then
@@ -1052,7 +1020,7 @@ function Start {
 
 	DetermineHostname
 	if [ "$KEY" == "" ] || [ "$SECRET" == "" ]
-	then	
+	then
 		Account
 	fi
 
@@ -1065,7 +1033,7 @@ function Start {
         	then
                 	Output
 		fi
-		
+
 		Cronjob
 	fi
 
@@ -1085,21 +1053,22 @@ function Login {
 
 			LOGIN_RET=($(ApiUserLogin "$EMAIL" "$PASSWORD"))
 
-			local LOGIN_CRITICAL=${LOGIN_RET[0]}
-			local LOGIN_TYPE=${LOGIN_RET[1]}
-			local LOGIN_AUTHED=${LOGIN_RET[2]}
-			local LOGIN_ERRORS=${LOGIN_RET[3]}
-			local LOGIN_USER=${LOGIN_RET[4]}
+			local LOGIN_ERROR=${LOGIN_RET[0]}
+			local LOGIN_USER=${LOGIN_RET[1]}
+			local LOGIN_KEY=${LOGIN_RET[2]}
+			local LOGIN_SECRET=${LOGIN_RET[3]}
 
-			if [ "$LOGIN_AUTHED" == "true" ]
+			if [ "$LOGIN_ERROR" == "false" ]
 			then
+				StoreKeySecret "$LOGIN_KEY" "$LOGIN_SECRET"
 				return
-			elif [ "$LOGIN_CRITICAL" == "true" ]
-			then
-				echo "> Your login was blocked for security issues, a mail was send to unblock yourself. After clicking on the link, you can try again." >&2
-			elif [ "$LOGIN_TYPE" == '"to_much_tries"' ]
+			elif [ "$LOGIN_ERROR" == '"too_many_failed_attempts"' ]
 			then
 				echo "> Your login was blocked for security issues, please try again in 10 min." >&2
+				exit 77
+			elif [ "$LOGIN_ERROR" == '"different_country"' ]
+			then
+				echo "> Your login is temporarily blocked for security measurements. Check your email for further instructions." >&2
 				exit 77
 			else
 				echo "> Wrong email and/or password! Try again." >&2
@@ -1110,37 +1079,37 @@ function Login {
 	else
 		if [ "$EMAIL" == "" ]
 		then
-			echo "Specify login credentials" >&2
+			echo "Specify login credentials." >&2
 			exit 77
 		fi
 
 		if [ "$PASSWORD" == "" ]
 		then
-			echo "Specify login credentials" >&2
+			echo "Specify login credentials." >&2
 			exit 77
 		fi
 
 		LOGIN_RET=($(ApiUserLogin "$EMAIL" "$PASSWORD"))
 
-		local LOGIN_CRITICAL=${LOGIN_RET[0]}
-		local LOGIN_TYPE=${LOGIN_RET[1]}
-		local LOGIN_AUTHED=${LOGIN_RET[2]}
-		local LOGIN_ERRORS=${LOGIN_RET[3]}
-		local LOGIN_USER=${LOGIN_RET[4]}
+		local LOGIN_ERROR=${LOGIN_RET[0]}
+                local LOGIN_USER=${LOGIN_RET[1]}
+                local LOGIN_KEY=${LOGIN_RET[2]}
+                local LOGIN_SECRET=${LOGIN_RET[3]}
 
-		if [ "$LOGIN_AUTHED" == "true" ]
+		if [ "$LOGIN_ERROR" == "false" ]
 		then
+			StoreKeySecret "$LOGIN_KEY" "$LOGIN_SECRET"
 			return
-		elif [ "$LOGIN_CRITICAL" == "true" ]
+		elif [ "$LOGIN_ERROR" == "8" ]
 		then
 			echo "Your login was blocked for security issues (Unblock mail send)." >&2
 			exit 77
-		elif [ "$LOGIN_TYPE" == '"to_much_tries"' ]
+		elif [ "$LOGIN_ERROR" == "9" ]
 		then
-			echo "Your login was blocked for security issues (Unblock mail send)" >&2
+			echo "Your login was blocked for security issues (Unblock mail send)." >&2
 			exit 77
 		else
-			echo "Login credentials incorrect" >&2
+			echo "Login credentials incorrect." >&2
 			exit 77
 		fi
 	fi
@@ -1148,19 +1117,39 @@ function Login {
   	exit 77;
 }
 
+function StoreKeySecret {
+	KEY="$1"
+	SECRET="$2"
+
+	if [ "$KEY" == "false" ]
+	then
+		echo "> Internal error, could not get key/secret combo." >&2
+		exit 77
+	fi
+
+	if [ "$SECRET" == "false" ]
+	then
+		echo "> Internal error, could not get key/secret combo." >&2
+		exit 77
+	fi
+}
+
 function Register {
 	REGISTER_RET=($(ApiUserRegister "$EMAIL" "$PASSWORD"))
 
-	local REGISTER_AUTHED=${REGISTER_RET[0]}
-	local REGISTER_ERRORS=${REGISTER_RET[1]}
-	local REGISTER_USER=${REGISTER_RET[2]}
+	local REGISTER_ERROR=${REGISTER_RET[0]}
+	local REGISTER_USER=${REGISTER_RET[1]}
+	local REGISTER_KEY=${REGISTER_RET[2]}
+	local REGISTER_SECRET=${REGISTER_RET[3]}
 
-	if [ "$REGISTER_AUTHED" == "true" ]
+	StoreKeySecret "$REGISTER_KEY" "$REGISTER_SECRET"
+
+	if [ "$REGISTER_ERROR" == "false" ]
 	then
 		echo "success"
 		return
 	else
-		if [[ "$REGISTER_ERRORS" =~ "The email has already been taken" ]]
+		if [[ "$REGISTER_ERROR" == "82" ]]
 		then
 			echo "email"
 			return
@@ -1206,11 +1195,11 @@ function Hostname {
 	then
 		if [ "$IP" == "" ]
 		then
-			echo "Hostname not found (Please enter with command)" >&2
+			echo "Hostname not found. (Please enter with command)" >&2
 			exit 77;
 		elif [[ "$IP" != "$EXTERNAL_IP" ]]
 		then
-			echo "Hostname doesn't resolve to external IP of this server" >&2
+			echo "Hostname doesn't resolve to external IP of this server." >&2
 			exit 77;
 		fi
 	fi
@@ -1240,44 +1229,8 @@ function Hostname {
 		fi
 	done
 
-	echo "> Could not determine hostname"  >&2
+	echo "> Could not determine hostname."  >&2
   	exit 77;
-}
-
-function GetKeySecret {
-	if [[ "$KEY" != "" ]] && [[ "$SECRET" != "" ]]
-	then
-		return
-	fi
-
-	# Get the first KEY/SECRET combo
-	KEY_SECRET_RET=($(ApiKeySecret))
-
-	KEY=${KEY_SECRET_RET[0]}
-	SECRET=${KEY_SECRET_RET[1]}
-
-	# API access isn't created yet, so create.
-	if [[ "$KEY" == "false" ]] || [[ "$SECRET" == "false" ]]
-	then	
-		ApiCreateKeySecret > /dev/null
-
-		KEY_SECRET_RET=($(ApiKeySecret))
-
-		KEY=${KEY_SECRET_RET[0]}
-		SECRET=${KEY_SECRET_RET[1]}
-	fi
-
-	if [ "$KEY" == "false" ]
-	then
-		echo "> Internal error, could not get key/secret combo" >&2
-		exit 77
-	fi
-
-	if [ "$SECRET" == "false" ]
-	then
-		echo "> Internal error, could not get key/secret combo" >&2
-		exit 77
-	fi
 }
 
 function DetermineHostname {
@@ -1295,7 +1248,7 @@ function DetermineHostname {
 		SERVER_EXISTS=${SERVER_EXISTS_RET[0]}
 		SERVER_EXISTS_ERROR_TYPE=${SERVER_EXISTS_RET[1]}
 		SERVER_EXISTS_ERRORS=${SERVER_EXISTS_RET[2]}
-		
+
 		if [ "$SERVER_EXISTS_ERROR_TYPE" == "false" ]
 		then
 			return
@@ -1305,7 +1258,6 @@ function DetermineHostname {
 		then
 			echo "> An account was already created for this host or a subdomain of this host. Please login into your patrolserver.com account or add the key/secret when calling this command." >&2
 			Login
-			GetKeySecret
 
 		# Hostname could not be found.
 		elif [ "$SERVER_EXISTS_ERROR_TYPE" == "2" ]
@@ -1325,16 +1277,15 @@ function Account {
 	if [[ "$EMAIL" != "" ]] && [[ "$PASSWORD" != "" ]]
 	then
 		Login
-		GetKeySecret
 		return;
 	fi
-	
-	if [[ "$CMD" != "false" ]] 
+
+	if [[ "$CMD" != "false" ]]
 	then
 		YN="n"
 	else
 		echo "> You can use this tool 5 times without account." 
-	
+
 		YN="..."
 		while [[ "$YN" != "n" ]] && [[ $YN != "y" ]]; do
 			read -rp "> Do you want to create an account (y/n)? " YN
@@ -1354,12 +1305,9 @@ function Account {
 			exit 77
 		fi
 
-		GetKeySecret
-
 	else
 		for I in 1 2 3
 		do
-		
 			# Ask what account should be created.
 			echo -en "\tYour email: "
 			read -r EMAIL
@@ -1376,7 +1324,6 @@ function Account {
 				REGISTER_RET=$(Register "$EMAIL" "$PASSWORD")
 				if [ "$REGISTER_RET" == "success" ]
 				then
-					GetKeySecret
 					return
 				fi
 			fi
